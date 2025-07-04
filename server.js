ebbe58c5ebe04d669a0c3d1275ac674d62abd014
Rosier Anderson <rosieranderson@Rosiers-Mac-mini.local>
2025-07-04 10:31:57 -0400

    Initial commit

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const PORT = process.env.PORT || 3500;
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method)
+
+    let path ;
+    
   
 })
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
