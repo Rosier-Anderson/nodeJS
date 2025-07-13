bbc2836584d8768f43e2c859c737cfdfcd6733dd
Anderson <97542626+Rosier-Anderson@users.noreply.github.com>
2025-07-13 07:49:10 +0000

    Refactor logging system: updated logEvents, removed old log file, added new logs

--- server.js
+++ server.js
@@ -2,19 +2,30 @@ const fs = require("fs");
 const fsPromises = require("fs").promises;
 const path = require("path");
 const http = require("http");
+const { logEvents } = require("./logEvents");
+const EventEmmiter = require("events");
 
+// start event
+class Emmiter extends EventEmmiter {}
+
+// the server
+const myEmmitter = new Emmiter();
+myEmmitter.on("logs", (msg) => logEvents(msg));
 const PORT = process.env.PORT || 3500;
 const serveFile = async (filePath, contentType, response) => {
   try {
     const data = await fsPromises.readFile(filePath, "utf-8");
-   response.writeHead(200, {"Content-Type" : contentType})
-   response.end(data)
-  } catch (err) {console.log(err)
+    response.writeHead(200, { "Content-Type": contentType });
+    response.end(data);
+  } catch (err) {
+    myEmmitter.emit("logs", err);
+    console.log(err);
     response.statusCode = 500;
-    response.end()
+    response.end();
   }
 };
 const server = http.createServer(async (req, res) => {
+  myEmmitter.emit("logs", `${req.url}\t${req.method}`);
   const extension = path.extname(req.url);
   let contentType;
   console.log(req.url, req.method);
@@ -46,16 +57,15 @@ const server = http.createServer(async (req, res) => {
   }
   let filePath =
     contentType === "text/html" && req.url === "/"
-     ? path.join(__dirname, "views", "index.html")
-      : contentType === "text/html" && req.url.slice(-1) === "/"
-     
-if(!extension && req.url.slice(-1) === '/') filePath += '.html'
+      ? path.join(__dirname, "views", "index.html")
+      : contentType === "text/html" && req.url.slice(-1) === "/";
+
+  if (!extension && req.url.slice(-1) !== "/") filePath += ".html";
   const fileExists = fs.existsSync(filePath);
   if (fileExists) {
- 
-    serveFile(filePath, contentType, res)
+    serveFile(filePath, contentType, res);
   } else {
- serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
+    serveFile(path.join(__dirname, "views", "404.html"), "text/html", res);
   }
 });
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
